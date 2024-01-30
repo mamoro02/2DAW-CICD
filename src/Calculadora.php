39c9816ee9a4cebf3a7754a04c93584eef9848b0
mery <mamoro02@floridaoberta.es>
2024-01-30 12:32:02 +0100

    resta

--- src/Calculadora.php
+++ src/Calculadora.php
@@ -3,29 +3,37 @@ class Calculadora
 {
     private $num1;
     private $num2;
-    
-    public function __construct($a1,$a2)
+
+    public function __construct($a1, $a2)
     {
-        $this->num1=$a1;
-        $this->num2=$a2;
-    }
-    
-    public function suma(){
-        return $this->num1+$this->num2;
+        $this->num1 = $a1;
+        $this->num2 = $a2;
     }
 
-    public function diff(){
-        return $this->num1-$this->num2;
+    public function suma()
+    {
 
+        return $this->num1 + $this->num2;
     }
 
-    public function mult(){
-        return $this->num1*$this->num2;
+    public function diff()
+    {
+        if ($this->num2 > $this->num1) {
+            $temp = $this->num1;
+            $this->num1 = $this->num2;
+            $this->num2 = $temp;
+        }
+
+        return $this->num1 - $this->num2;
     }
 
-    public function div(){
-        return $this->num1/$this->num2;
-        
+    public function mult()
+    {
+        return $this->num1 * $this->num2;
+    }
+
+    public function div()
+    {
+        return $this->num1 / $this->num2;
     }
 }
-?>
--- tests/CalculadoraTest.php
+++ tests/CalculadoraTest.php
@@ -1,33 +1,30 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
+
 include './src/Calculadora.php';
 
 final class CalculadoraTest extends TestCase
 {
     public function testSuma()
     {
-        $this->assertEquals(1,1);
-        /*$calc = new Calculadora(3,5);
-        $this->assertEquals(8, $calc->suma());*/
+        $calc = new Calculadora(3,5);
+        $this->assertEquals(8, $calc->suma());
     }
 
     public function testDiff()
     {
-        $this->assertEquals(1,1);
-        
+        $calc = new Calculadora(3,5);
+        $this->assertEquals(2, $calc->diff());
     }
 
     public function testMult()
     {
-        $this->assertEquals(1,1);
-      
+        $this->assertEquals(1, 1);
     }
 
     public function testDiv()
     {
-        $this->assertEquals(1,1);
-        
+        $this->assertEquals(1, 1);
     }
-
 }
-?>
